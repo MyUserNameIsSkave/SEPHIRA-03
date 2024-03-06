2b3b549359a76ee55ed4c82b99cafe1c46e6bf5b
POLI Enzo <vaycks@outlook.fr>
2024-03-06 21:48:10 +0100

    Réglage d'un soucis de reference
    Impossible de réferencer des Elements d'une scene dans un Scriptable Object. Déplacement d'Array dans le Script de management des dialogues. Initialisation automatique de ces Array.

--- Assets/Content/TEST ZONE/DialogueData.cs
+++ Assets/Content/TEST ZONE/DialogueData.cs
@@ -10,7 +10,4 @@ public class DialogueData : ScriptableObject
 
     public AudioClip[] audioLine;
     public SerializedDictionary<string[], float[]> subtitles;       // The Lines of Sub to play with lines
-    public DialogueManager[] nextInterlocutor;                      // Must use IEventTriggerable - Either Next Dialogue or External Event
-    public MonoBehaviour[] eventToTrigger;                          // Must use IEventTriggerable - Either Next Dialogue or External Event
-
 }
--- Assets/Content/TEST ZONE/DialogueManager.cs
+++ Assets/Content/TEST ZONE/DialogueManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour, IEventTriggerable
 {
     //VARIABLES
 
+    [Header ("  DEBUG")]
     [SerializeField]
     private int audioIndex = 0;
 
     [SerializeField]
     private int subtitbleIndex = 0;
 
+    [SerializeField]
+    private AudioSource audioSource;
 
+
+
+    [Space (40)]
 
 
 
@@ -20,9 +27,47 @@ public class DialogueManager : MonoBehaviour, IEventTriggerable
     private DialogueData dialogueData = null;
 
 
+    [Space (20)]
+    [Header ("  DONT CHANGE LENGHT")]
+    public DialogueManager[] nextInterlocutor;                      // Must use IEventTriggerable - Either Next Dialogue or External Event
+    public MonoBehaviour[] eventToTrigger;                          // Must use IEventTriggerable - Either Next Dialogue or External Event
 
-    [SerializeField]
-    private AudioSource audioSource;
+
+
+
+    private void OnValidate()
+    {
+
+        //Validity Check
+        if (dialogueData == null)
+        {
+            return;
+        }
+
+        if (dialogueData.audioLine == null)
+        {
+            return;
+        }
+
+
+
+        //Arrays Creation
+
+        if (dialogueData.audioLine.Length != nextInterlocutor.Length)
+        {
+            nextInterlocutor = new DialogueManager[dialogueData.audioLine.Length];
+        }
+
+        if (dialogueData.audioLine.Length != eventToTrigger.Length)
+        {
+            eventToTrigger = new MonoBehaviour[dialogueData.audioLine.Length];
+        }
+
+    }
+
+
+
+
 
 
 
