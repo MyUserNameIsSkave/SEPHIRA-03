df5c6131d5c73728dccb9810de1d998c1cec37e1
POLI Enzo <vaycks@outlook.fr>
2024-03-24 20:01:25 +0100

    Indicateur de la rotation de base d'une camera

--- Assets/Content/Logic/UI/UiCamerBars.cs
+++ Assets/Content/Logic/UI/UiCamerBars.cs
@@ -4,16 +4,39 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class UiCamerBars : MonoBehaviour
 {
-    
+
     public static UiCamerBars Instance;
 
     [SerializeField]
-    private Slider topSlider, leftSlider, zoomSlider;
+    private Slider topSlider;
+
+    [SerializeField]
+    private Slider leftSlider;
+
+    [SerializeField]
+    private Slider zoomSlider;
+
+
+    [Space(20)]
+
+
+    [SerializeField]
+    private Slider topBase;
+
+    [SerializeField]
+    private Slider leftBase;
+
+    [SerializeField]
+    private Slider zoomBase;
+
+
 
 
     private CameraController cameraController;
+    private CameraBase currentCamera;
 
 
 
@@ -27,28 +50,50 @@ public class UiCamerBars : MonoBehaviour
 
 
 
+
+
+
+
+
+
+
     public void UpdateUI()
     {
-        StartCoroutine(updateUI());
+        StartCoroutine(UpdateBarsPositions());
+        print("camera switch");
+
     }
 
 
 
-    IEnumerator updateUI()
+    IEnumerator UpdateBarsPositions()
     {
-        topSlider.value = (cameraController.CurrentCamera.BaseYaw - cameraController.CurrentCamera.HorizontalRange.x) / (Mathf.Abs(cameraController.CurrentCamera.HorizontalRange.y) + Mathf.Abs(cameraController.CurrentCamera.HorizontalRange.x));
-        yield return new WaitForNextFrameUnit();
 
-        leftSlider.value = (cameraController.CurrentCamera.BasePitch + cameraController.CurrentCamera.VerticalRange.y) / (Mathf.Abs(cameraController.CurrentCamera.VerticalRange.y) + Mathf.Abs(cameraController.CurrentCamera.VerticalRange.x));
-        yield return new WaitForNextFrameUnit();
+        if (currentCamera != cameraController.CurrentCamera)
+        {
+            currentCamera = cameraController.CurrentCamera;
+            UpdateBaseIndicator();
+        }
 
-        zoomSlider.value = cameraController.CurrentCamera.currentCameraFOV / (cameraController.CurrentCamera.FOVRange.x + cameraController.CurrentCamera.FOVRange.y);
-        yield return new WaitForNextFrameUnit();
+
+
+        topSlider.value = (currentCamera.BaseYaw - currentCamera.HorizontalRange.x) / (Mathf.Abs(currentCamera.HorizontalRange.y) + Mathf.Abs(currentCamera.HorizontalRange.x));
+        leftSlider.value = (currentCamera.BasePitch + currentCamera.VerticalRange.y) / (Mathf.Abs(currentCamera.VerticalRange.y) + Mathf.Abs(currentCamera.VerticalRange.x));
+        zoomSlider.value = currentCamera.currentCameraFOV / (currentCamera.FOVRange.x + currentCamera.FOVRange.y);
+
+        yield return null;
     }
 
 
 
+    private void UpdateBaseIndicator()
+    {
 
+        //Creer une variable dans les camera qui retient la position de base qui lui est attribué et récupérer celle la plutot que la position actuelle
+        //pour faire en sorte de baser la positio nde l'indicateurr sur la vrais position de base plus que sur la posiition d'arrivé sur une camera
 
-
+        topBase.value = (currentCamera.BaseYaw - currentCamera.HorizontalRange.x) / (Mathf.Abs(currentCamera.HorizontalRange.y) + Mathf.Abs(currentCamera.HorizontalRange.x));
+        leftBase.value = (currentCamera.BasePitch + currentCamera.VerticalRange.y) / (Mathf.Abs(currentCamera.VerticalRange.y) + Mathf.Abs(currentCamera.VerticalRange.x));
+        zoomBase.value = currentCamera.currentCameraFOV / (currentCamera.FOVRange.x + currentCamera.FOVRange.y);
+    }
 }
