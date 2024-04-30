659f1e961c4055471577b4e22aa7b4a4e96afe2c
POLI Enzo <vaycks@outlook.fr>
2024-04-30 15:57:15 +0200

    Radio Implementation

---
+++ Assets/Content/Logic/Interactions/Interactions/Player/PI_Radio.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+
+public class PI_Radio : Player_Interaction
+{
+    
+    private AudioSource _audioSource;
+
+
+
+    [Header("    SETTINGS")]
+    [Space(15)]
+
+    [SerializeField]
+    private bool isOn = true;
+
+    [SerializeField]
+    private AudioClip[] _audioClips;
+
+    [SerializeField]
+    private int musicTrack;
+
+    [SerializeField]
+    private float onVolume= 1f;
+
+
+
+
+
+
+
+
+
+
+    private void Awake()
+    {
+        _audioSource.clip = _audioClips[musicTrack + 1];
+        Apply();
+    }
+
+
+
+
+    public override void Interaction()
+    {
+        Switch();
+    }
+
+    public override void TriggerEvent()
+    {
+        Switch();
+    }
+
+
+
+    private void Switch()
+    {
+        isOn = !isOn;
+        Apply();
+    }
+
+    private void Apply()
+    {
+        if (isOn)
+        {
+            _audioSource.volume = onVolume;
+        }
+        else
+        {
+            _audioSource.volume = 0.0f;
+
+        }
+    }
+
+}
