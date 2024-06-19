4594120c089a4e94c86bb76b2f418a66881d6cad
MAHEO Loick <loick.maheo@brassart.fr>
2024-06-19 03:36:47 +0200

    General Audio Intégration Part 1

---
+++ Assets/Content/Assets/Audio/General/Scripts/FootSteps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+        Debug.Log("Bruit de pas");
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
