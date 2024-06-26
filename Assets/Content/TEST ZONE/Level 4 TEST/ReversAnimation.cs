0d39099aab27c44299e1a16573bf06db512a3050
POLI Enzo <vaycks@outlook.fr>
2024-06-26 13:41:33 +0200

    debug block keter

--- Assets/Content/TEST ZONE/Level 4 TEST/ReversAnimation.cs
+++ Assets/Content/TEST ZONE/Level 4 TEST/ReversAnimation.cs
@@ -11,6 +11,7 @@ public class ReversAnimation : MonoBehaviour
     Animator animator;
 
     bool isBeingReversed= false;
+    bool triggeredOnce = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,29 +22,59 @@ public class ReversAnimation : MonoBehaviour
 
     private void FixedUpdate()
     {
- 
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        if (triggeredOnce && !isBeingReversed)
         {
             animator.SetFloat("AnimationSpeed", 0);
+            return;
+        }
+
+        if (triggeredOnce && isBeingReversed)
+        {
+            animator.SetFloat("AnimationSpeed", reverseSpeed);
+            return;
+        }
+
+
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        {
+            if (!isBeingReversed)
+            {
+                animator.SetFloat("AnimationSpeed", 0);
+            }
+            else
+            {
+                animator.SetFloat("AnimationSpeed", reverseSpeed);
+            }
         }
         else
         {
-            animator.SetFloat("AnimationSpeed", 1);
+            if (!isBeingReversed)
+            {
+                animator.SetFloat("AnimationSpeed", 1);
+
+            }
         }
-        
     }
 
 
 
     private void OnMouseDrag()
     {
-        isBeingReversed = true; 
-        animator.SetFloat("AnimationSpeed", reverseSpeed);
+        isBeingReversed = true;
+        //animator.SetFloat("AnimationSpeed", reverseSpeed);
     }
 
     private void OnMouseUp()
     {
         isBeingReversed = false;
-        animator.SetFloat("AnimationSpeed", 1);
+        //animator.SetFloat("AnimationSpeed", 0);
+    }
+
+    private void OnMouseDown()
+    {
+        triggeredOnce = true;
+
+        isBeingReversed = true;
+        //animator.SetFloat("AnimationSpeed", 0);
     }
 }
