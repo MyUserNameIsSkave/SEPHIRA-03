3e2887b64d2dfc65a89ddfe9249c2279fb545b71
MAHEO Loick <loick.maheo@brassart.fr>
2024-06-29 14:52:13 +0200

    LVL 1 Foule ChangeAudioSource

---
+++ Assets/Content/Assets/Audio/Level 1/Ascenseur/ChangeAudioSource.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAudioSource : MonoBehaviour, IEventTriggerable
+{
+    public AudioClip clip; // Clip audio
+
+    private AudioSource audioSource; // Référence à l'AudioSource
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); 
+    }
+
+    public void TriggerEvent()
+    {
+        audioSource.clip = clip; // Définit le nouveau clip à jouer
+        audioSource.Play(); // Joue le nouveau clip
+    }
+
+}
